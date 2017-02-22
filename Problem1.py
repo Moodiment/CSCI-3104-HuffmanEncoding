5bd2809cde666989de2501fa1f6569bebd486372
Bryan <copperclock@gmail.com>
2017-02-22 12:06:45 -0700

    Init

---
+++ Problem1.py
@@ -0,0 +1,22 @@
+#Bryan Brent
+#Problemset 5
+#Algorithms CSC-3104
+
+def minHeap(): # TODO: What input paremeters do we need?
+
+
+def string2freq(stringX): #stringX is a string of symbols from alphabet S
+    # TODO: Part (i)
+    #list of unique symbols in stringX, in lexicographic order.
+    S =
+    # histogram of frequencies of symbols in stringX, same order.
+    f =
+    return S, f
+
+def huffmanencode(S,f):
+    # TODO: Part (ii)
+
+
+
+def encodeString(x,T):
+    # TODO: Part (iii)

8699973e914391a267a0885bb74b970e376b6530
Bryan <copperclock8@gmail.com>
2017-02-22 22:47:46 -0700

    Testing before Counter

---
+++ ProblemSet5.py
@@ -0,0 +1,29 @@
+from collections import Counter
+
+def string2freq(x): # x is a string of symbols from alphabet S
+    S = Counter(x)
+
+    for index, i in enumerate(x):
+        if i not in S:
+            S.append(i)
+            f.append(1)
+        elif i in S:
+            list(x).count(i)
+
+
+    x.count(i for i in x)
+    return(S,f)
+
+
+
+def huffmanEncode(S, f): # f is a vector of symbol frequencies, from above
+    #TODO: Heap = minheap() // initialize minHeap
+    print('s')
+
+
+
+def encodeString():
+    # TODO:
+    print('s')
+
+print(string2freq("aaaasssdddddd"))

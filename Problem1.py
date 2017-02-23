8f4330b7a7bb5065a60c9a2170d25f4675976322
DackJempsey <jade8185@colorado.edu>
2017-02-23 11:53:33 -0700

    part II and III changed
    Many of the changes were commenting and such

--- Problem1.py
+++ Problem1.py
@@ -5,36 +5,36 @@ def string2freq(x): # x is a string of symbols from alphabet S
     #store numbers as (key, value) = (leter, frequency)
 
 		#insert:
-    for i in (len(x) - 1):
-        if (s[i/2] == s[i]): increment f[i]
+    for i in (len(x) - 1): #increment by i/2, do not want to check all of the inputs
+        if (s[i/2] == s[i]): f[i]+=1
 
-		if (s[i/2] < S[i]):
+        if (s[i/2] < S[i]): 
 			#swap the terms
 
-	#historgram of frequencies of symbols in x, same order:
-	f=vector(frequencies) #shorthand
+            #historgram of frequencies of symbols in x, same order:
+    f=vector(frequencies) #shorthand
 
-	return S, f;
+    return S, f;
 
 
 def huffmanEncode(S, f): # f is a vector of symbol frequencies, from above
-	H.empty() #initialize H
-	for i=1 in n : H.insert(i,f[i])
-	for k=n+1 in 2n-1 :
-		i = H.deletmin()
-		j= H.deletmin()
+#using a fixed length code
+    H.empty() #initialize H
+    #buiding the huffman tree from bottom up, but in an array
+    n=len(S)
+    for i in n : H.insert(i,f[i])
+    k=n+1
+    for k in 2*n-1 :
+        i = H.deletmin()  # pop the right child
+        j=  H.deletmin()   #pop the left child
 		#creat a node numbered k with children i,j
-		f[k] = f[i]+f[j]
-		H.insert(k,f[k])
-	#creat t and return in
-	return T
+        f[k] = f[i]+f[j] # frequencies added together
+        H.insert(k,f[k])
+        #creat T the codebook and return it
+        T=[len(H)]
+    return T
 
-
-	def encodeString(x,T):
-	y = #empty string
-	for i=1 to x.len() : y += T[x[i]] # encode each symbol of x
-	return y
-
-
-def encodeString(x,T):ghvgh
-    # TODO: Part (iii)
+def encodeString(x,T):
+    y = ""#empty string
+    for i in x.len() : y += T[x[i]] # encode each symbol of x
+    return y

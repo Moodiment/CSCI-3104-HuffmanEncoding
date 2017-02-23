74f02e2b8e55f7e4ef090411ff0d1845e7937a0c
Bryan <copperclock@gmail.com>
2017-02-23 01:09:06 -0700

    Working String2freq

--- ProblemSet5.py
+++ ProblemSet5.py
@@ -1,13 +1,11 @@
 from collections import Counter,defaultdict
 
 def string2freq(x): # x is a string of symbols from alphabet.
-    S = sorted(list(x))
+    S = sorted(set(x))
     f = list()
 
-    for character in set(S):
-        S.count(character)
-    for character in set(S):
-        f.append(S.count(character))
+    for character in S:
+        f.append(x.count(character))
 
     return S, f
 
@@ -15,10 +13,8 @@ def huffmanEncode(S, f): # f is a vector of symbol frequencies, from above
     #TODO: Heap = minheap() // initialize minHeap
     print('s')
 
-
-
 def encodeString():
     # TODO:
     print('s')
 
-print(string2freq("aasssdddda"))
+print(string2freq("aaasssssssssdd"))

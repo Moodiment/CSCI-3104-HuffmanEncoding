d9bd63f1ff93182a786937dcfdadd153b2212d7e
Bryan <copperclock8@gmail.com>
2017-02-22 18:30:53 -0700

    Changes

--- Problem1.py
+++ Problem1.py
@@ -1,22 +1,36 @@
-#Bryan Brent
-#Problemset 5
-#Algorithms CSC-3104
-
-def minHeap(): # TODO: What input paremeters do we need?
 
 
-def string2freq(stringX): #stringX is a string of symbols from alphabet S
-    # TODO: Part (i)
-    #list of unique symbols in stringX, in lexicographic order.
-    S =
-    # histogram of frequencies of symbols in stringX, same order.
-    f =
-    return S, f
+def string2freq(x): # x is a string of symbols from alphabet S
+    S=vector(x) #list of unique symbols not yet in lexigraphical priority binary heap in an vector
+    #store numbers as (key, value) = (leter, frequency)
 
-def huffmanencode(S,f):
-    # TODO: Part (ii)
+		#insert:
+    for i in (len(x) - 1):
+        if (s[i/2] == s[i]): increment f[i]
+
+		if (s[i/2] < S[i]):
+			#swap the terms
+
+	#historgram of frequencies of symbols in x, same order:
+	f=vector(frequencies) #shorthand
+
+	return S, f;
 
 
+def huffmanEncode(S, f): # f is a vector of symbol frequencies, from above
+	H.empty() #initialize H
+	for i=1 in n : H.insert(i,f[i])
+	for k=n+1 in 2n-1 :
+		i = H.deletmin()
+		j= H.deletmin()
+		#creat a node numbered k with children i,j
+		f[k] = f[i]+f[j]
+		H.insert(k,f[k])
+	#creat t and return in
+	return T
 
-def encodeString(x,T):
-    # TODO: Part (iii)
+
+	def encodeString(x,T):
+	y = #empty string
+	for i=1 to x.len() : y += T[x[i]] # encode each symbol of x
+	return y

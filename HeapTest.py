4a655e56dd12c74ca2f2069492445b30c36b7387
Bryan <copperclock8@gmail.com>
2017-02-25 13:37:50 -0700

    Refactoring

--- HeapTest.py
+++ HeapTest.py
@@ -17,7 +17,7 @@ class MinHeap:
 
     def BubbleDown(self,i):
       while (i * 2) <= self.currentSize: #While loop to move down the tree.
-          mc = self.minChild(i) #
+          mc = self.FindChild(i) #
           if self.hList[i][0] > self.hList[mc][0]:
               tmp = self.hList[i]
               self.hList[i] = self.hList[mc]
@@ -29,7 +29,7 @@ class MinHeap:
       self.currentSize = self.currentSize + 1 #Incriment the Heap
       self.BubbleUp(self.currentSize) # Because we added the tuple to the end of the list, we need to bubble it up
 
-    def minChild(self,i):
+    def FindChild(self,i):
       if i * 2 + 1 > self.currentSize:
           return i * 2
       else:

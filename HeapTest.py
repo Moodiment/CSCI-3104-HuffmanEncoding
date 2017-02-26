63b767ebf183b0576dd16e68d17a9798732e4b89
Bryan <copperclock@gmail.com>
2017-02-25 18:02:26 -0700

    Node works in Heap

--- HeapTest.py
+++ HeapTest.py
@@ -7,20 +7,25 @@ class Node:
         self.rightNode = rightNode
         self.leftNode = leftNode
 
+        def setLeftNode(self, leftNode):
+            self.leftNode = leftNode
+        def setRightNode(self, rightNode):
+            self.rightNode = rightNode
+
 class MinHeap:
     def __init__(self): #Constructor
         # Initalize a list to used for tuple
-        self.hList = Node(-1,0,None,None)
+        self.hList = [0]
         # Current size of heap.
         self.currentSize = 0
 
     def BubbleUp(self,i):
         while i // 2 > 0: #While loop to recursively move up the tree.
-          if self.hList[i].freq < self.hList[i // 2].freq: #Check to see if parent is greater than child
-             temp = self.hList[i // 2] #Save parent in temporary
-             self.hList[i // 2] = self.hList[i] #Assign child as parent
-             self.hList[i] = temp #Assign parent as child.
-          i = i // 2 #Move to next height.
+            if self.hList[i].freq < self.hList[i // 2].freq: #Check to see if parent is greater than child
+                temp = self.hList[i // 2] #Save parent in temporary
+                self.hList[i // 2] = self.hList[i] #Assign child as parent
+                self.hList[i] = temp #Assign parent as child.
+            i = i // 2 #Move to next height.
 
     def BubbleDown(self,i):
       while (i * 2) <= self.currentSize: #While loop to move down the tree.
@@ -31,8 +36,8 @@ class MinHeap:
               self.hList[mc] = tmp
           i = mc
 
-    def insert(self, Node):
-      self.hList.append(Node) #Add the tuple, frequency and character to the end of the Heap
+    def insert(self, Node1):
+      self.hList.append(Node1) #Add the tuple, frequency and character to the end of the Heap
       self.currentSize = self.currentSize + 1 #Incriment the Heap
       self.BubbleUp(self.currentSize) # Because we added the tuple to the end of the list, we need to bubble it up
 
@@ -40,7 +45,7 @@ class MinHeap:
       if i * 2 + 1 > self.currentSize:
           return i * 2
       else:
-          if self.hList[i*2][0] < self.hList[i*2+1][0]:
+          if self.hList[i*2].freq < self.hList[i*2+1].freq:
               return i * 2
           else:
               return i * 2 + 1
@@ -56,15 +61,4 @@ class MinHeap:
 
 bh = MinHeap()
 
-#bh.buildHeap(["a","s","d"])
-
-"""
-for x in range(randrange(20)):
-    bh.insert(x,"s")
-
-
-
-print(bh.delMin())
-print(bh.delMin())
-print(bh.delMin())
-"""
+bh.insert
--- ProblemSet5.py
+++ ProblemSet5.py
@@ -2,13 +2,22 @@ from collections import Counter,defaultdict
 from HeapTest import MinHeap, Node
 import DFTInorder
 
-x="the road not taken by robert frost two roads diverged in a yellow wood, and sorry i could not travel both and be one traveler, long i stood and looked down one as far as i could to where it bent in the undergrowth; then took the other, as just as fair, and having perhaps the better claim, because it was grassy and wanted wear; though as for that the passing there had worn them really about the same, and both that morning equally lay in leaves no step had trodden black. oh, i kept the first for another day! yet knowing how way leads on to way, i doubted if i should ever come back. i shall be telling this with a sigh somewhere ages and ages hence: two roads diverged in a wood, and i- i took the one less traveled by, and that has made all the difference."
+read_in_string="the road not taken by robert frost two roads diverged in a yellow wood, and \
+sorry i could not travel both and be one traveler, long i stood and looked down\
+ one as far as i could to where it bent in the undergrowth; then took the other,\
+  as just as fair, and having perhaps the better claim, because it was grassy \
+  and wanted wear; though as for that the passing there had worn them really \
+  about the same, and both that morning equally lay in leaves no step had trodden\
+   black. oh, i kept the first for another day! yet knowing how way leads on to \
+   way, i doubted if i should ever come back. i shall be telling this with a sigh\
+    somewhere ages and ages hence: two roads diverged in a wood, and i- i took \
+    the one less traveled by, and that has made all the difference."
 
-def string2freq(x): # x is a string of symbols from alphabet.
-    S = sorted(set(x)) #Can I use this?
+def string2freq(read_string): # x is a string of symbols from alphabet.
+    S = sorted(set(read_string)) #Can I use this?
     f = list()
     for character in S:
-        f.append(x.count(character))
+        f.append(read_string.count(character))
     return S, f
 
 def huffmanEncode(S, f): # f is a vector of symbol frequencies, from above
@@ -17,38 +26,40 @@ def huffmanEncode(S, f): # f is a vector of symbol frequencies, from above
     H = MinHeap() # H.initialize
     n = len(f)
     for i in range(n):
-        H.insert(f[i],S[i]) #Insert frequency, characater
-
-    k = n+1
-    n = 2*n-1
-    for k in range(n+1, 2*n-1):
-        i = H.delMin() #Return a tuple from the lowest element
-        j = H.delMin()
-
-        # f[k] = f[i] + f[j]
-        TempNode = Node(i[0]+j[0],-1,i,j)
-        print(TempNode)
-
+        new_node = Node(f[i], S[i], None, None)
+        H.insert(new_node) #Insert frequency, characater
+        print(new_node.freq)
+        print(new_node.char)
         input()
-
-    T = []
-    for i in n:
-        T.append(DFTInOrder(H, i))
-
-    return T
+    #
+    # k = n+1
+    # n = 2*n-1
+    # for k in range(n+1, 2*n-1):
+    #     i = H.delMin() #Return a tuple from the lowest element
+    #     j = H.delMin()
+    #
+    #     # f[k] = f[i] + f[j]
+    #     TempNode = Node(i[0]+j[0],-1,i,j)
+    #     print(TempNode)
+    #
+    #     input()
+    #
+    # T = []
+    # for i in n:
+    #     T.append(DFTInOrder(H, i))
+    #
+    # return T
 
 def encodeString():
     return
 
 
-#print(string2freq(x))
-
-S, f = string2freq(x)
+S, f = string2freq(read_in_string)
+#print(str(S)+" " + str(f))
 huffmanEncode(S, f)
 
-print(huffmanEncode)
 
 
-#Testhing...
+#Testing...
 
 #print(string2freq("asd!!!$$###fasdas"))

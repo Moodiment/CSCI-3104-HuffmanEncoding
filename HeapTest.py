2e1c34c7f8751d20b43543ac5a29396bbfe0015f
Bryan <brbr6964@colorado.edu>
2017-03-06 12:08:53 -0700

    Refactoring

--- HeapTest.py
+++ HeapTest.py
@@ -17,6 +17,11 @@ class MinHeap:
         self.hList = [0] #We must start with one element in the heap for integer division
         # Current size of heap.
         self.currentSize = 0
+    
+    def insert(self, Node1):
+        self.hList.append(Node1) #Add the tuple, frequency and character to the end of the Heap
+        self.currentSize = self.currentSize + 1 #Incriment the Heap
+        self.BubbleUp(self.currentSize) # Because we added the tuple to the end of the list, we need to bubble it up
 
     def BubbleUp(self,i):
         while i // 2 > 0: #While loop to recursively move up the tree.
@@ -35,27 +40,20 @@ class MinHeap:
               self.hList[mc] = tmp
           i = mc
 
-    def insert(self, Node1):
-      self.hList.append(Node1) #Add the tuple, frequency and character to the end of the Heap
-      self.currentSize = self.currentSize + 1 #Incriment the Heap
-      self.BubbleUp(self.currentSize) # Because we added the tuple to the end of the list, we need to bubble it up
-
     def FindChild(self,i):
       if i * 2 + 1 > self.currentSize:
           return i * 2
-      else:
-          if self.hList[i*2].freq < self.hList[i*2+1].freq:
+      else: if self.hList[i*2].freq < self.hList[i*2+1].freq:
               return i * 2
-          else:
-              return i * 2 + 1
+          else: return i * 2 + 1
 
     def delMin(self):
-      retval = self.hList[1] #Pops the top value off the que
+      getmin = self.hList[1] #Pops the top value off the que
       self.hList[1] = self.hList[self.currentSize] #Collects the last item in the que and replaces it with the popped item
       self.currentSize = self.currentSize - 1 #Decrements current size cause we're deleting
       self.hList.pop() # Pops lowest value off list
       self.BubbleDown(1) #Bubbles d own the first element in the list
-      return retval
+      return getmin
 
 
 bh = MinHeap()
